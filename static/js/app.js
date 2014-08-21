6125aaca9bc73dcf03aa194e41c5f4d38cfb7a12
Nick Tzaperas <nick@nijotz.com>
2014-08-21 01:30:43 -0700

    More model relationship improvements

--- static/js/app.js
+++ static/js/app.js
@@ -18,29 +18,28 @@ App.Router.map(function() {
 
 App.Artist = DS.Model.extend({
     name: DS.attr('string'),
-    albums: DS.hasMany('album'),
-    non_album_songs: DS.hasMany('song')
+    albums: DS.hasMany('album', {async:true}),
+    non_album_songs: DS.hasMany('song', {async:true})
 });
 
 App.Album = DS.Model.extend({
     name: DS.attr('string'),
-    artist: DS.belongsTo('artist')
+    artist: DS.belongsTo('artist'),
+    songs: DS.hasMany('song', {async:true})
 });
 
 App.Song = DS.Model.extend({
-    uri: DS.attr('string'),
-    artist: DS.attr('string'),
-    album: DS.attr('string'),
-    title: DS.attr('string'),
-    length: DS.attr('string'),
-    playing: DS.attr('boolean'),
     artist: DS.belongsTo('artist'),
     album: DS.belongsTo('album'),
+    title: DS.attr('string'),
+    length: DS.attr('string'),
+    file: DS.attr('string'),
+    playing: DS.attr('boolean'),
     queue: DS.belongsTo('queue')
 });
 
 App.Queue = DS.Model.extend({
-    songs: DS.hasMany('song')
+    songs: DS.hasMany('song', {async:true})
 })
 
 App.ArtistsRoute = Ember.Route.extend({
