9c73278bdd82c1100b9a24ed4ca741a8bfb6e094
Nick Tzaperas <nick@nijotz.com>
2014-09-01 15:28:19 -0700

    Updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,85 +1,39 @@
-// Karma configuration
-// Generated on Fri Aug 29 2014 15:50:37 GMT-0700 (PDT)
-
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    basePath: 'static/js',
     frameworks: ['qunit'],
-
     plugins: [
       'karma-qunit',
       'karma-chrome-launcher',
       'karma-ember-preprocessor',
       'karma-phantomjs-launcher'
     ],
-
-    // list of files / patterns to load in the browser
     files: [
-      'static/js/lib/jquery-1.10.2.js',
-      'static/js/lib/bootstrap.min.js',
-      'static/js/lib/handlebars-1.1.2.js',
-      'static/js/lib/ember-1.7.0.js',
-      'static/js/lib/ember-data-1.0.0-beta9.js',
-      'static/js/lib/socket.io-0.9.16.js',
-      'static/js/app.js',
-      'static/js/tests/**/*.js'
+      'lib/jquery-1.10.2.js',
+      'lib/bootstrap.min.js',
+      'lib/handlebars-1.1.2.js',
+      'lib/ember-1.7.0.js',
+      'lib/ember-data-1.0.0-beta9.js',
+      'lib/socket.io-0.9.16.js',
+      'lib/jquery-mockjax-1.5.4.js',
+      'app.js',
+      'tests/*.js',
+      'templates/*.hbs'
     ],
-
-
-    // list of files to exclude
-    exclude: [
-      
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    exclude: [ ],
     preprocessors: {
-    
+      '**/*.hbs': 'ember'
     },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress'],
-
-
-    // web server port
     port: 9876,
-
-    // proxies
     proxies: {
         '/api': 'http://localhost:5000/api',
         '/static': 'http://localhost:5000/static',
     },
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
+    singleRun: false
   });
 };
