999b6e8464e7f0e3fc5bbe9cca4e98794368cf04
Nick Tzaperas <nick@nijotz.com>
2014-09-01 16:02:37 -0700

    mockjax cannot currently handle cross domain requests

--- static/js/tests/integration_tests.js
+++ static/js/tests/integration_tests.js
@@ -9,11 +9,6 @@ module('integration tests', {
 
 
 test('github commits pulls 5 commits', function() {
-    var json = $.ajax({
-        url: '/static/js/tests/fixtures/github.json',
-        async: false,
-    });
-    stubEndpoint('/', json);
     visit("/");
     andThen(function() {
         var rows = find("#github-commits > li").length;
