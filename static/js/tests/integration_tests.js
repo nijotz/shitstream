1c7496edcd88dab3d05a05df9d09a364463a6fda
Nick Tzaperas <nick@nijotz.com>
2014-09-01 15:31:29 -0700

    Fixed simple github test

--- static/js/tests/integration_tests.js
+++ static/js/tests/integration_tests.js
@@ -4,15 +4,19 @@ module('integration tests', {
             App.reset();
         });
     },
-    teardown: function() {
-    }
+    teardown: function() { }
 });
 
 
 test('github commits pulls 5 commits', function() {
+    var json = $.ajax({
+        url: '/static/js/tests/fixtures/github.json',
+        async: false,
+    });
+    stubEndpoint('/', json);
     visit("/");
     andThen(function() {
-        var rows = find("#github-commits").length;
+        var rows = find("#github-commits > li").length;
         equal(rows, 5);
     });
 });
