aa35633e5b9accf0d07ac2c917fc8a470e9d0f1d
Nick Tzaperas <nick@nijotz.com>
2014-09-01 20:43:27 -0700

    Added test for queueing songs

--- static/js/tests/integration_tests.js
+++ static/js/tests/integration_tests.js
@@ -7,11 +7,30 @@ module('integration tests', {
     teardown: function() { }
 });
 
-
 test('github commits pulls 5 commits', function() {
     visit("/");
     andThen(function() {
         var rows = find("#github-commits > li").length;
         equal(rows, 5);
+    });
+});
+
+test('adding song to queue will show up on queue page', function() {
+    visit("/music/artists");
+    andThen(function() {
+        click('#artist-list .list-group-item:first');
+    });
+    andThen(function() {
+        click('.album-link:first');
+    });
+    andThen(function() {
+        click('.queue-btn:first');
+    });
+    andThen(function() {
+        click('#nav-link-queue a');
+    });
+    andThen(function() {
+        var songs = find('.playlist-song').length;
+        equal(songs > 0, true);
     });
 });
